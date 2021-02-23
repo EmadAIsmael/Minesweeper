556d5ff39ca5b92c2208cf0ca9535be915e7ac29
Emad A. Ismael <EmadAIsmael@yahoo.com>
2021-02-23 11:18:58 +0200

    Tue., Feb. 23rd, 2021 11:18

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,17 +1,38 @@
 package minesweeper
 
+import kotlin.random.Random
+
 
 class MineField {
 
     val rows = 9
     val columns = 9
-    val field = Array(9) { Array(9) { '.' } }
+    var field = Array(rows) { Array(columns) { '.' } }
 
     fun setMine(x: Int, y: Int) {
         if (x in 1..rows && y in 1..columns)
             field[x-1][y-1] = 'X'
         else
             println("row $x and column $y should be from 1 to 9")
+    }
+
+    fun isFreeCell(r: Int, c: Int) = field[r - 1][c - 1] == '.'
+
+    fun newField() {
+        print("How many mines do you want on the field? > ")
+        val numMines = readLine()!!.toInt()
+
+        field = Array(rows) { Array(columns) { '.' } }
+        var count = 0
+        while (count < numMines) {
+            val r = Random.nextInt(1, rows + 1)
+            val c = Random.nextInt(1, columns + 1)
+
+            if (isFreeCell(r, c)) {
+                setMine(r, c)
+                count++
+            }
+        }
     }
 
     override fun toString(): String {
@@ -29,11 +50,6 @@ class MineField {
 fun main() {
     val mf = MineField()
 
-    mf.setMine(1,1)
-    mf.setMine(5,5)
-    mf.setMine(9,9)
-    // mf.setMine(10, 56)
-    mf.setMine(7, 5)
-
+    mf.newField()
     println(mf)
 }
